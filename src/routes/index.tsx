2ba9a549ccdf83290992e34251f26d958af5a720
Alireza Safaierad <alireza.safaierad@gmail.com>
2026-01-04 21:22:35 +0100

    feat: add noise

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,7 @@
+import { IconAssembly } from '@tabler/icons-react';
 import { createFileRoute } from '@tanstack/react-router';
 
-import { Button } from '../ui/Button/Button';
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '../ui/Card/Card';
+import { Card, CardDescription, CardHeader, CardTitle } from '../ui/Card/Card';
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -16,24 +10,15 @@ export const Route = createFileRoute('/')({
 function App() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Card className="relative w-full max-w-sm overflow-hidden pt-0">
-        <div className="absolute inset-0 z-30 aspect-video bg-primary opacity-50 mix-blend-color" />
-        <img
-          src="https://images.unsplash.com/photo-1604076850742-4c7221f3101b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="Mymind on Unsplash"
-          className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale"
-        />
+      <Card className="noise w-full max-w-sm">
         <CardHeader>
-          <CardTitle>Observability Plus is replacing Monitoring</CardTitle>
-          <CardDescription>
-            Switch to the improved way to explore your data, with natural
-            language. Monitoring will no longer be available on the Pro plan in
-            November, 2025
+          <CardTitle className="flex items-center gap-1 text-muted-foreground">
+            <IconAssembly className="size-4" /> Slot
+          </CardTitle>
+          <CardDescription className="text-disabled-foreground">
+            Add new routine
           </CardDescription>
         </CardHeader>
-        <CardFooter>
-          <div className="ml-auto">Warning</div>
-        </CardFooter>
       </Card>
     </div>
   );
