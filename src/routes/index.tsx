cf561eac0dd0e5e06c6de18335c0dd34bfd27501
Alireza Safaierad <alireza.safaierad@gmail.com>
2026-01-04 21:02:22 +0100

    feat: add card

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router';
 
 import { Button } from '../ui/Button/Button';
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../ui/Card/Card';
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -9,7 +16,25 @@ export const Route = createFileRoute('/')({
 function App() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Button>Hello</Button>
+      <Card className="relative w-full max-w-sm overflow-hidden pt-0">
+        <div className="absolute inset-0 z-30 aspect-video bg-primary opacity-50 mix-blend-color" />
+        <img
+          src="https://images.unsplash.com/photo-1604076850742-4c7221f3101b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Mymind on Unsplash"
+          className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale"
+        />
+        <CardHeader>
+          <CardTitle>Observability Plus is replacing Monitoring</CardTitle>
+          <CardDescription>
+            Switch to the improved way to explore your data, with natural
+            language. Monitoring will no longer be available on the Pro plan in
+            November, 2025
+          </CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <div className="ml-auto">Warning</div>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
---
+++ src/ui/Card/Card.stories.tsx
@@ -0,0 +1,37 @@
+import preview from '#storybook/preview';
+
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './Card';
+
+const meta = preview.meta({
+  component: Card,
+  render: args => (
+    <Card className="relative w-full max-w-sm overflow-hidden pt-0" {...args}>
+      <div className="absolute inset-0 z-30 aspect-video bg-primary opacity-50 mix-blend-color" />
+      <img
+        src="https://images.unsplash.com/photo-1604076850742-4c7221f3101b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Mymind on Unsplash"
+        className="relative aspect-video w-full object-cover brightness-60 grayscale"
+      />
+      <CardHeader>
+        <CardTitle>Observability Plus is replacing Monitoring</CardTitle>
+        <CardDescription>
+          Switch to the improved way to explore your data, with natural
+          language. Monitoring will no longer be available on the Pro plan in
+          November, 2025
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <div className="ml-auto">Warning</div>
+      </CardFooter>
+    </Card>
+  ),
+  tags: ['autodocs'],
+});
+
+export const Default = meta.story();
